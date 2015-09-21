de4bb67216d81b46ebe144e7bdbebbb8cfb1195e
Kenneth van Ewijk <kennyboy@live.nl>
2015-09-21 12:58:01 +0200

    Implemented most common classes

---
+++ ErgometerLibrary/NetCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErgometerLibrary
+{
+    class NetCommand
+    {
+
+        public int Session { get; set; }
+        public string DisplayName { get; set; }
+        public double Timestamp { get; set; }
+
+        public NetCommand(int session, string displayname)
+        {
+            Session = session;
+            DisplayName = displayname;
+            Timestamp = (DateTime.Now - DateTime.Parse("1/1/1870 0:0:0")).TotalMilliseconds;
+        }
+    }
+}
