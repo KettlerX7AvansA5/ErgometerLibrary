063264b1e3953894e962ec2d5024270c2daa97a8
Kenneth van Ewijk <kennyboy@live.nl>
2015-10-02 11:54:49 +0200

    Added extra methods for string sending and receiving

--- ErgometerLibrary/NetHelper.cs
+++ ErgometerLibrary/NetHelper.cs
@@ -14,13 +14,28 @@ namespace ErgometerLibrary
             client.GetStream().Flush();
         }
 
+        public static void SendString(TcpClient client, string command)
+        {
+            byte[] b = Encoding.ASCII.GetBytes(command);
+            client.GetStream().Write(b, 0, b.Length);
+            client.GetStream().Flush();
+        }
+
         public static NetCommand ReadNetCommand(TcpClient client)
         {
             byte[] bytesFrom = new byte[(int) client.ReceiveBufferSize];
             client.GetStream().Read(bytesFrom, 0, (int)client.ReceiveBufferSize);
-            String response = Encoding.ASCII.GetString(bytesFrom);
+            string response = Encoding.ASCII.GetString(bytesFrom);
             NetCommand net = NetCommand.Parse(response);
             return net;
+        }
+
+        public static string ReadString(TcpClient client)
+        {
+            byte[] bytesFrom = new byte[(int)client.ReceiveBufferSize];
+            client.GetStream().Read(bytesFrom, 0, (int)client.ReceiveBufferSize);
+            string response = Encoding.ASCII.GetString(bytesFrom);
+            return response;
         }
 
         public static IPAddress GetIP(string ipstring)
