3cc8b70009dfe7b95ac92edbe34868711bce6159
Kenneth van Ewijk <kennyboy@live.nl>
2015-10-20 22:48:40 +0200

    Moved local ip to library

--- ErgometerLibrary/NetHelper.cs
+++ ErgometerLibrary/NetHelper.cs
@@ -99,5 +99,20 @@ namespace ErgometerLibrary
 
             return ip;
         }
+
+        public static IPAddress GetIP()
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                }
+            }
+            return GetIP(localIP);
+        }
     }
 }
